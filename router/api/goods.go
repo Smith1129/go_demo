9647f08340215e0ac14a120fa4c13cccb70b03f6
zzz <1006472680@qq.com>
2020-01-14 18:34:10 +0800

    'aaa'

--- router/api/goods.go
+++ router/api/goods.go
@@ -3,6 +3,8 @@ package api
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"go_demo/models"
+
 	//"go_demo/models"
 	"go_demo/utils"
 	"net/http"
@@ -10,13 +12,13 @@ import (
 )
 
 func GetGoodList(c *gin.Context){
-	//var good []models.Good
+	var good []models.Good
 	page,err := strconv.Atoi(c.DefaultQuery("Page","1"))
 	if err != nil{
 		fmt.Println(err)
 	}
 	//pageSize := 10
-	result := utils.Paginator(5,page,"good")
+	result := utils.Paginator(5,page,"good",good)
 	//fmt.Println(page,pageSize,page*pageSize)
 	////分页 limit ->返回多少个数据 offset从第几个数据开始
 	//global.GormConfig.Limit(10).Offset(5).Find(&good)
--- utils/page.go
+++ utils/page.go
@@ -1,10 +1,13 @@
 package utils
 
 import (
+	"fmt"
 	"go_demo/global"
+	"reflect"
 )
 
-func Paginator(pageSize int,currentPage int,model string)  map[string] interface{}{
+func Paginator(pageSize int,currentPage int,model string,target interface{})  map[string] interface{}{
+	fmt.Println(reflect.TypeOf(target),"-----------------")
 	//tablename,err := json.Marshal(model)
 	//fmt.Println(tablename,err)
 	//获取总数据
@@ -13,9 +16,9 @@ func Paginator(pageSize int,currentPage int,model string)  map[string] interface
 	//var data interface{}
 	global.GormConfig.Table(model).Count(&count)
 	//分页 limit ->返回多少个数据 offset从第几个数据开始
-	global.GormConfig.Limit(pageSize).Offset(currentPage*pageSize).Find(&mapresult)
-	//mapresult["Count"] = count
-	//mapresult["Page"] = currentPage
-	//mapresult["list"] = target
+	global.GormConfig.Limit(pageSize).Offset(currentPage*pageSize).Find(reflect.ValueOf(&target))
+	mapresult["Count"] = count
+	mapresult["Page"] = currentPage
+	mapresult["list"] = &target
 	return mapresult
 }
